1bdc6168704e15662cfe8b679fdbd52a98d77326
surya <varma.surya@protonmail.com>
2024-07-22 11:34:03 +0530

    configure our react react app with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="font-bold">Hello Namastae React</h1>
+    </div>
+  );
+};
+
+export default App;
