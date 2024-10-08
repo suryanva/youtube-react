30ad964a10a336fc0c3ba6f65dd9c87205c282b1
surya <varma.surya@protonmail.com>
2024-10-08 16:44:35 -0400

    Fix YouTube search API URL in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?par
 }`;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const LIVE_CHAT_COUNT = 15;
